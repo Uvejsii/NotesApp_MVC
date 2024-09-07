8fbb552fbcf61a6d13940cb3d8e4f7af91f52c65
Uvejsii <136826106+Uvejsii@users.noreply.github.com>
2024-09-07 19:28:55 +0200

    Add project files.

---
+++ NotesApp_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NotesApp_MVC.Models;
+
+namespace NotesApp_MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ NotesApp_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NotesApp_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
